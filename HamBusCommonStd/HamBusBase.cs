69b6681556e40ef1b764f06beb3e7b64bfa9ecc4
Darryl Wagoner <darryl.wagoner@statellite.com>
2020-08-02 18:37:05 -0500

    Initial commit with RigState DTO

---
+++ HamBusCommonStd/HamBusBase.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace HamBusCommonStd.Model
+{
+  public class HamBusBase
+  {
+    static public long SerialNum_ { get; set; } = 1;
+
+    public long SerialNum
+    {
+      get { return SerialNum_; }
+    }
+    public long SerialNumDym { get; set; }
+
+    //public override string ToString()
+    //{
+    //  var options = new JsonSerializerOptions
+    //  {
+    //    //PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    //    WriteIndented = true
+    //  };
+
+    //  string output = JsonSerializer.Serialize(this, options);
+    //  Console.WriteLine($"ToString output: {output}");
+    //  return output;
+    //}
+    public virtual void IncSerial()
+    {
+      SerialNum_++;
+      SerialNumDym = SerialNum_;
+    }
+  }
+}
