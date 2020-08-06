730a4580b9b65319b5374edff1c15e7264198d53
Darryl Wagoner <darryl.wagoner@statellite.com>
2020-08-05 21:17:09 -0500

    added new model

---
+++ HamBusCommonStd/BusStatusModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HamBusCommonStd
+{
+  public class BusStatusModel
+  {
+
+    public string Name { get; set; }
+    public ActiveBusesModel ActiveBus { get; set; }
+    public RigState State { get; set; }
+  }
+}
