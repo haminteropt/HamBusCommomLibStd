16539d7b671fb6e16568fff57f657bcc61ae5b4b
Darryl Wagoner <darryl.wagoner@statellite.com>
2020-09-12 21:40:19 -0500

    added serilog

---
+++ HamBusCommonStd/KenwoodTypes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HamBusCommonStd
+{
+  public static class KenwoodTypes
+  {
+    public enum ModeValues
+    {
+      /// <summary> Defines the LSB
+      /// </summary>
+      LSB = 1,
+      /// <summary> Defines the USB
+      /// </summary>
+      USB = 2,
+      /// <summary> Defines the CW
+      /// </summary>
+      CW = 3,
+      /// <summary> Defines the FM
+      /// </summary>
+      FM = 4,
+      /// <summary> Defines the AM
+      /// </summary>
+      AM = 5,
+      /// <summary> Defines the FSK
+      /// </summary>
+      FSK = 6,
+      /// <summary>// Defines the CWR
+      /// </summary>
+      CWR = 7,
+      /// <summary> Defines the Tune
+      /// </summary>
+      Tune = 8,
+      /// <summary> Defines the FSR
+      /// </summary>
+      FSKR = 9,
+      /// <summary> Defines the ERROR
+      /// </summary>
+      ERROR = 10
+    }
+  }
+}
